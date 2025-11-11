24e591764d4e33d76bbefb4578d36a0b31446471
OzgurDevFolio <odemirbacak4985@gmail.com>
2025-11-11 09:35:19 +0300

    Implementing auth layouts

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import FooterLink from '@/components/forms/FooterLink'
+import InputField from '@/components/forms/inputField'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+const SignIn = () => {
+    const router = useRouter()
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitting },
+    } = useForm<SignInFormData>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onBlur',
+    })
+    const onSubmit = async (data: SignInFormData) => {
+        try {
+            const result = await signInWithEmail(data)
+            if (result.success) router.push('/')
+        } catch (error) {
+            console.error(error)
+            toast.error('Sign in failed', {
+                description: error instanceof Error ? error.message : 'Failed to sign in',
+            })
+        }
+    }
+
+    return (
+        <>
+            <h1 className="form-title">Welcome Back</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                    register={register}
+                    error={errors.email}
+                    validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email address is required' }}
+                />
+                <InputField
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{ required: 'Password is required', minLength: 8 }}
+                />
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? 'Signing in' : 'Sign in'}
+                </Button>
+                <FooterLink text="Don't have an account?" href="/sign-up" linkText="Sign up" />
+            </form>
+        </>
+    )
+}
+
+export default SignIn
